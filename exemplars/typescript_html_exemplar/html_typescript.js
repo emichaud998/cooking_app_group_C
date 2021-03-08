ea1a752a001ef55d058dd91da6eef465af18988e
emichaud998 <59289146+emichaud998@users.noreply.github.com>
2021-03-08 10:41:19 -0500

    Add more comments to the html_typescript exemplar

--- exemplars/typescript_html_exemplar/html_typescript.js
+++ exemplars/typescript_html_exemplar/html_typescript.js
@@ -34,13 +34,17 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
+// Event listener for the loading of the HTML page
 window.addEventListener('load', startHTML);
 function startHTML() {
     return __awaiter(this, void 0, void 0, function () {
         return __generator(this, function (_a) {
             switch (_a.label) {
-                case 0: return [4 /*yield*/, addEventListeners()];
+                case 0: 
+                // Add event listeners to HTML input elements
+                return [4 /*yield*/, addEventListeners()];
                 case 1:
+                    // Add event listeners to HTML input elements
                     _a.sent();
                     return [2 /*return*/];
             }
@@ -52,23 +56,16 @@ function addEventListeners() {
         var checkboxCards, textInputButton, firstNameInput, checkboxButton, radioButtons, _loop_1, i;
         return __generator(this, function (_a) {
             checkboxCards = document.getElementById('checkboxCards');
-            if (checkboxCards != null) {
-                checkboxCards.addEventListener('click', cardCheckboxClick);
-            }
+            checkboxCards.addEventListener('click', cardCheckboxClick);
             textInputButton = document.getElementById('text_input_button');
-            if (textInputButton != null) {
-                textInputButton.addEventListener('click', inputSelect);
-            }
+            textInputButton.addEventListener('click', inputSelect);
             firstNameInput = document.getElementById('firstNameInput');
-            if (firstNameInput != null) {
-                firstNameInput.addEventListener('keydown', textColorChange);
-            }
+            firstNameInput.addEventListener('keydown', textColorChange);
             checkboxButton = document.getElementById('checkbox_button');
-            if (checkboxButton != null) {
-                checkboxButton.addEventListener('click', checkboxClick);
-            }
+            checkboxButton.addEventListener('click', checkboxClick);
             radioButtons = document.getElementsByName('radiogroup');
             _loop_1 = function (i) {
+                // To call a function that requires an argument to be passed in, define a function that calls the function you need with arguments passed in
                 radioButtons[i].addEventListener('click', function () { radioSelect(radioButtons[i]); });
             };
             for (i = 0; i < radioButtons.length; i++) {
@@ -78,12 +75,18 @@ function addEventListeners() {
         });
     });
 }
+// radioSelect function simply displays an alert dialog saying which radio button has been clicked
 function radioSelect(element) {
+    // element is the radio button element that was clicked- it's value has been set in the HTML to the name of the radio button
     window.alert(element.value + ' selected!');
 }
+// checkboxClick function grabs all checkbox elements in group (below Checkboxes header) and displays alert dialog saying which checkboxes have been checked
 function checkboxClick() {
+    // Grab list of all checkboxes with name checkboxgroup in HTML
     var checkboxGroup = document.getElementsByName('checkboxgroup');
     var checkboxString = "";
+    // Loop through each checkbox HTML input element and check if it has been checked (if checked boolean attribute is true)
+    // If checked, add the elements value (name of checkbox in this example) to output string
     for (var i = 0; i < checkboxGroup.length; i++) {
         if (checkboxGroup[i].checked) {
             checkboxString = checkboxString + checkboxGroup[i].value + " ";
@@ -96,24 +99,29 @@ function checkboxClick() {
         window.alert('Selected checkboxes: ' + checkboxString);
     }
 }
+// inputSelect function grabs the first name and last name text input HTML input elements and displays alert dialog message with the first name and last name values
 function inputSelect() {
+    // Grab the first name and last name HTMLInputElements from HTML 
     var firstNameInput = document.getElementById('firstNameInput');
     var lastNameInput = document.getElementById('lastNameInput');
+    // Grab value from the text inputs (string typed into text input box)
     var firstName = firstNameInput.value;
     var lastName = lastNameInput.value;
     window.alert('Hello ' + firstName + ' ' + lastName + '!');
 }
+// textColorChange changes the color of the font in the text input box from green to blue or blue to green
 function textColorChange() {
     var firstNameInput = document.getElementById('firstNameInput');
-    if (firstNameInput !== null) {
-        if (firstNameInput.style.color !== "green") {
-            firstNameInput.style.color = "green";
-        }
-        else {
-            firstNameInput.style.color = "blue";
-        }
+    // Check the current color of the text with style.color property
+    if (firstNameInput.style.color !== "green") {
+        // Set new color by setting style.color equal to new color (string)
+        firstNameInput.style.color = "green";
+    }
+    else {
+        firstNameInput.style.color = "blue";
     }
 }
+// cardCheckboxClick function checks if the checkbox after being clicked has been checked- calls addCardRow() or unchecked- calls deleteCardRow()
 function cardCheckboxClick() {
     var checkBox = document.getElementById('checkboxCards');
     if (checkBox.checked) {
@@ -123,49 +131,71 @@ function cardCheckboxClick() {
         deleteCardRow();
     }
 }
+// addCardRow function shows a variety of examples of how to add new HTML elements to existing HTML- adds new row of card elements
 function addCardRow() {
+    // To add new elements to HTML, first need to grab the parent node that the new elements will be added to
     var parentContainer = document.getElementById('card_container');
-    if (parentContainer !== null) {
-        var cardRow = document.createElement('div');
-        cardRow.classList.add('card-deck', 'row', 'mb-3');
-        cardRow.id = "cardRowAdd";
-        for (var i = 0; i < 3; i++) {
-            var card = document.createElement('div');
-            card.classList.add('card');
-            var cardBody = document.createElement('div');
-            cardBody.classList.add('card-body');
-            cardBody.id = "card" + (i + 4).toString();
-            var image = document.createElement('img');
-            image.src = "https://picsum.photos/300/200?random=" + (i + 4).toString();
-            cardBody.appendChild(image);
-            var h5Title = document.createElement('h5');
-            h5Title.innerText = "Card " + (i + 4).toString();
-            h5Title.classList.add("card-title");
-            cardBody.appendChild(h5Title);
-            var h6Title = document.createElement('h6');
-            h6Title.innerText = "A nice looking card.";
-            h6Title.classList.add("card-subtitle", "mb-2", "text-muted");
-            cardBody.appendChild(h6Title);
-            var pElement = document.createElement('p');
-            pElement.classList.add('card-text');
-            pElement.innerText = "This is more text explaining more about what the card is referencing, such as a recipe.";
-            cardBody.appendChild(pElement);
-            var a = document.createElement('a');
-            a.href = "#";
-            a.innerText = "Link to learn more.";
-            cardBody.appendChild(a);
-            card.appendChild(cardBody);
-            cardRow.appendChild(card);
-        }
-        parentContainer.appendChild(cardRow);
+    // Create a new div element which in this case will be the div that holds a row of card elements
+    var cardRow = document.createElement('div');
+    // Add classes to the cardRow div element
+    cardRow.classList.add('card-deck', 'row', 'mb-3');
+    // Set the id of the cardRow div element
+    cardRow.id = "cardRowAdd";
+    // Adding 3 new cards to the cardRow div element
+    for (var i = 0; i < 3; i++) {
+        // Create card div element- will hold the body of the card
+        var card = document.createElement('div');
+        card.classList.add('card');
+        // Add id to each card starting with card4 going up to card6
+        card.id = "card" + (i + 4).toString();
+        // Create card body div element- will hold all the pictures and text within the card
+        var cardBody = document.createElement('div');
+        cardBody.classList.add('card-body');
+        // Create image element
+        var image = document.createElement('img');
+        // Set the image source to the following random picture url
+        image.src = "https://picsum.photos/300/200?random=" + (i + 4).toString();
+        // Add the image element to the cardBody div
+        cardBody.appendChild(image);
+        // Create a new h5 (title) element
+        var h5Title = document.createElement('h5');
+        // Add text to the h5 element (this is the text that will be displayed by this header element)
+        h5Title.innerText = "Card " + (i + 4).toString();
+        h5Title.classList.add("card-title");
+        // Add the h5 element to the cardBody div (adds to the end of the cardBody div after image element)
+        cardBody.appendChild(h5Title);
+        // Create a new h6 (title) element, add text to be displayed by this title, add classes
+        var h6Title = document.createElement('h6');
+        h6Title.innerText = "A nice looking card.";
+        h6Title.classList.add("card-subtitle", "mb-2", "text-muted");
+        // Add the h6 element to the cardBody div (adds to the end of the cardBody div)
+        cardBody.appendChild(h6Title);
+        // Create new p element (paragraph)
+        var pElement = document.createElement('p');
+        pElement.classList.add('card-text');
+        // Add text for the paragraph element to display
+        pElement.innerText = "This is more text explaining more about what the card is referencing, such as a recipe.";
+        // Add the paragraph element to the cardBody div (adds to the end of the cardBody div)
+        cardBody.appendChild(pElement);
+        // Create new a (hyperlink) element
+        var a = document.createElement('a');
+        // Define the hyperlink url by setting .href attribute
+        a.href = "#";
+        // Add text for the hyperlink
+        a.innerText = "Link to learn more.";
+        // Add the hyperlink element to the cardBody div (adds to the end of the cardBody div)
+        cardBody.appendChild(a);
+        // Add the cardBody div element (which contains all the images and text from above) to the card element
+        card.appendChild(cardBody);
+        // Add the card element (which now contains the cardBody) to the cardRow div
+        cardRow.appendChild(card);
     }
+    // After all 3 cards have been created and added to the cardRow div, add this new row of cards to the parent div
+    parentContainer.appendChild(cardRow);
 }
+// deleteCardRow shows how to delete existing HTML nodes from HTML- deletes added row of cards
 function deleteCardRow() {
     var parentContainer = document.getElementById('card_container');
-    if (parentContainer !== null) {
-        var childNode = document.getElementById('cardRowAdd');
-        if (childNode !== null) {
-            parentContainer.removeChild(childNode);
-        }
-    }
+    var childNode = document.getElementById('cardRowAdd');
+    parentContainer.removeChild(childNode);
 }
--- exemplars/typescript_html_exemplar/html_typescript.ts
+++ exemplars/typescript_html_exemplar/html_typescript.ts
@@ -1,45 +1,51 @@
+// Event listener for the loading of the HTML page
 window.addEventListener('load', startHTML);
 
 async function startHTML() {
+    // Add event listeners to HTML input elements
     await addEventListeners();
 }
 
 async function addEventListeners() {
-    let checkboxCards = document.getElementById('checkboxCards')
-    if (checkboxCards != null) {
-        checkboxCards.addEventListener('click', cardCheckboxClick);
-    }
+    // Add event listener to the checkbox above the HTML cards- on click call cardCheckboxClick()
+    let checkboxCards = document.getElementById('checkboxCards') as HTMLElement
+    checkboxCards.addEventListener('click', cardCheckboxClick);
 
-    let textInputButton = document.getElementById('text_input_button')
-    if (textInputButton != null) {
-        textInputButton.addEventListener('click', inputSelect);
-    }
+    // Add event listener to submit button below firstname/lastname text inputs- on click call inputSelect()
+    let textInputButton = document.getElementById('text_input_button') as HTMLElement
+    textInputButton.addEventListener('click', inputSelect);
 
-    let firstNameInput = document.getElementById('firstNameInput')
-    if (firstNameInput != null) {
-        firstNameInput.addEventListener('keydown', textColorChange);
-    }
+    // Add event listener to the firstname input- on key down call textColorChange()
+    let firstNameInput = document.getElementById('firstNameInput') as HTMLElement
+    firstNameInput.addEventListener('keydown', textColorChange);
 
-    let checkboxButton = document.getElementById('checkbox_button')
-    if (checkboxButton != null) {
-        checkboxButton.addEventListener('click', checkboxClick)
-    }
+
+    // Add event listener to the submit button below checkboxes- on click call checkboxClick
+    let checkboxButton = document.getElementById('checkbox_button') as HTMLElement
+    checkboxButton.addEventListener('click', checkboxClick)
     
-
-    let radioButtons = <NodeListOf<HTMLInputElement>>  document.getElementsByName('radiogroup')
+    // Radio buttons are a group of HTML input elements- for each radio button add event listener- on click call a function that calls radioSelect with clicked radio button element passed in
+    let radioButtons = document.getElementsByName('radiogroup') as NodeListOf<HTMLInputElement>
     for (let i = 0; i < radioButtons.length; i++) {
+        // To call a function that requires an argument to be passed in, define a function that calls the function you need with arguments passed in
         radioButtons[i].addEventListener('click', () => {radioSelect(radioButtons[i])});
     }
 }
 
+// radioSelect function simply displays an alert dialog saying which radio button has been clicked
 function radioSelect(element: HTMLInputElement) {
+    // element is the radio button element that was clicked- it's value has been set in the HTML to the name of the radio button
     window.alert(element.value + ' selected!')
 }
 
+// checkboxClick function grabs all checkbox elements in group (below Checkboxes header) and displays alert dialog saying which checkboxes have been checked
 function checkboxClick() {
-    let checkboxGroup= <NodeListOf<HTMLInputElement>> document.getElementsByName('checkboxgroup');
+    // Grab list of all checkboxes with name checkboxgroup in HTML
+    let checkboxGroup= document.getElementsByName('checkboxgroup') as NodeListOf<HTMLInputElement>;
     let checkboxString = ""
 
+    // Loop through each checkbox HTML input element and check if it has been checked (if checked boolean attribute is true)
+    // If checked, add the elements value (name of checkbox in this example) to output string
     for (let i = 0; i < checkboxGroup.length; i++) {
         if (checkboxGroup[i].checked) {
             checkboxString = checkboxString + checkboxGroup[i].value +" "
@@ -54,10 +60,13 @@ function checkboxClick() {
 
 }
 
+// inputSelect function grabs the first name and last name text input HTML input elements and displays alert dialog message with the first name and last name values
 function inputSelect() {
-    let firstNameInput = <HTMLInputElement>  document.getElementById('firstNameInput')
-    let lastNameInput = <HTMLInputElement>  document.getElementById('lastNameInput')
+    // Grab the first name and last name HTMLInputElements from HTML 
+    let firstNameInput = document.getElementById('firstNameInput') as HTMLInputElement
+    let lastNameInput = document.getElementById('lastNameInput') as HTMLInputElement
     
+    // Grab value from the text inputs (string typed into text input box)
     let firstName = firstNameInput.value;
     let lastName = lastNameInput.value;
 
@@ -65,19 +74,21 @@ function inputSelect() {
 
 }
 
+// textColorChange changes the color of the font in the text input box from green to blue or blue to green
 function textColorChange() {
-    let firstNameInput = document.getElementById('firstNameInput');
-    if (firstNameInput !== null) {
-        if (firstNameInput.style.color !== "green") {
-            firstNameInput.style.color = "green"
-        } else {
-            firstNameInput.style.color = "blue"
-        }
+    let firstNameInput = document.getElementById('firstNameInput') as HTMLElement;
+    // Check the current color of the text with style.color property
+    if (firstNameInput.style.color !== "green") {
+        // Set new color by setting style.color equal to new color (string)
+        firstNameInput.style.color = "green"
+    } else {
+        firstNameInput.style.color = "blue"
     }
 }
 
+// cardCheckboxClick function checks if the checkbox after being clicked has been checked- calls addCardRow() or unchecked- calls deleteCardRow()
 function cardCheckboxClick() {
-    let checkBox = <HTMLInputElement> document.getElementById('checkboxCards');
+    let checkBox = document.getElementById('checkboxCards') as HTMLInputElement;
     if (checkBox.checked) {
         addCardRow()
     } else {
@@ -85,64 +96,87 @@ function cardCheckboxClick() {
     }
 }
 
+// addCardRow function shows a variety of examples of how to add new HTML elements to existing HTML- adds new row of card elements
 function addCardRow() {
-    let parentContainer = document.getElementById('card_container');
+    // To add new elements to HTML, first need to grab the parent node that the new elements will be added to
+    let parentContainer = document.getElementById('card_container') as HTMLElement;
 
-    if (parentContainer !== null) {
-        let cardRow = document.createElement('div');
-        cardRow.classList.add('card-deck', 'row', 'mb-3')
-        cardRow.id = "cardRowAdd"
-        for (let i = 0; i < 3; i++) {
-            let card = document.createElement('div');
-            card.classList.add('card');
+    // Create a new div element which in this case will be the div that holds a row of card elements
+    let cardRow = document.createElement('div');
+    // Add classes to the cardRow div element
+    cardRow.classList.add('card-deck', 'row', 'mb-3')
+    // Set the id of the cardRow div element
+    cardRow.id = "cardRowAdd"
 
-            let cardBody = document.createElement('div');
-            cardBody.classList.add('card-body');
-            cardBody.id = "card" + (i + 4).toString()
+    // Adding 3 new cards to the cardRow div element
+    for (let i = 0; i < 3; i++) {
+        // Create card div element- will hold the body of the card
+        let card = document.createElement('div');
+        card.classList.add('card');
+        // Add id to each card starting with card4 going up to card6
+        card.id = "card" + (i + 4).toString()
 
-            let image = document.createElement('img');
-            image.src = "https://picsum.photos/300/200?random=" + (i + 4).toString()
+        // Create card body div element- will hold all the pictures and text within the card
+        let cardBody = document.createElement('div');
+        cardBody.classList.add('card-body');
 
-            cardBody.appendChild(image)
-            
-            let h5Title = document.createElement('h5');
-            h5Title.innerText = "Card " + (i + 4).toString();
-            h5Title.classList.add("card-title");
+        // Create image element
+        let image = document.createElement('img');
+        // Set the image source to the following random picture url
+        image.src = "https://picsum.photos/300/200?random=" + (i + 4).toString()
 
-            cardBody.appendChild(h5Title)
+        // Add the image element to the cardBody div
+        cardBody.appendChild(image)
+        
+        // Create a new h5 (title) element
+        let h5Title = document.createElement('h5');
+        // Add text to the h5 element (this is the text that will be displayed by this header element)
+        h5Title.innerText = "Card " + (i + 4).toString();
+        h5Title.classList.add("card-title");
 
-            let h6Title = document.createElement('h6');
-            h6Title.innerText = "A nice looking card.";
-            h6Title.classList.add("card-subtitle", "mb-2", "text-muted");
+        // Add the h5 element to the cardBody div (adds to the end of the cardBody div after image element)
+        cardBody.appendChild(h5Title)
 
-            cardBody.appendChild(h6Title)
+        // Create a new h6 (title) element, add text to be displayed by this title, add classes
+        let h6Title = document.createElement('h6');
+        h6Title.innerText = "A nice looking card.";
+        h6Title.classList.add("card-subtitle", "mb-2", "text-muted");
 
-            let pElement = document.createElement('p');
-            pElement.classList.add('card-text')
-            pElement.innerText = "This is more text explaining more about what the card is referencing, such as a recipe."
+        // Add the h6 element to the cardBody div (adds to the end of the cardBody div)
+        cardBody.appendChild(h6Title)
 
-            cardBody.appendChild(pElement)
+        // Create new p element (paragraph)
+        let pElement = document.createElement('p');
+        pElement.classList.add('card-text')
+        // Add text for the paragraph element to display
+        pElement.innerText = "This is more text explaining more about what the card is referencing, such as a recipe."
 
-            let a = document.createElement('a');
-            a.href = "#";
-            a.innerText = "Link to learn more.";
+        // Add the paragraph element to the cardBody div (adds to the end of the cardBody div)
+        cardBody.appendChild(pElement)
 
-            cardBody.appendChild(a)
+        // Create new a (hyperlink) element
+        let a = document.createElement('a');
+        // Define the hyperlink url by setting .href attribute
+        a.href = "#";
+        // Add text for the hyperlink
+        a.innerText = "Link to learn more.";
 
-            card.appendChild(cardBody)
-            cardRow.appendChild(card)
-        }
+        // Add the hyperlink element to the cardBody div (adds to the end of the cardBody div)
+        cardBody.appendChild(a)
 
-        parentContainer.appendChild(cardRow)
+        // Add the cardBody div element (which contains all the images and text from above) to the card element
+        card.appendChild(cardBody)
+        // Add the card element (which now contains the cardBody) to the cardRow div
+        cardRow.appendChild(card)
     }
+
+    // After all 3 cards have been created and added to the cardRow div, add this new row of cards to the parent div
+    parentContainer.appendChild(cardRow)
 }
 
+// deleteCardRow shows how to delete existing HTML nodes from HTML- deletes added row of cards
 function deleteCardRow() {
-    let parentContainer = document.getElementById('card_container');
-    if (parentContainer !== null) {
-        let childNode = document.getElementById('cardRowAdd')
-        if (childNode !== null) {
-            parentContainer.removeChild(childNode)
-        }
-    }
+    let parentContainer = document.getElementById('card_container') as HTMLElement;
+    let childNode = document.getElementById('cardRowAdd') as HTMLElement;
+    parentContainer.removeChild(childNode)
 }
