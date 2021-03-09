2f7c4ce426c966fc50bc676675ad6a9b2f14d25d
Andrew Leger <andrewleger9709@gmail.com>
2021-03-08 20:38:09 -0500

    ESLint Exemplar

---
+++ exemplars/eslint_and_launchJson_exemplar/lib/tutorial.js
@@ -0,0 +1,5 @@
+"use strict";
+// Try running this when you finish your launch.json file
+console.log('hello world');
+var x = 'hello';
+//# sourceMappingURL=tutorial.js.map
---
+++ exemplars/eslint_and_launchJson_exemplar/src/tutorial.ts
@@ -0,0 +1,5 @@
+// Try running this when you finish your launch.json file
+console.log('hello world');
+
+
+let x = 'hello';
